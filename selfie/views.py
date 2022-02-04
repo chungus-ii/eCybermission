348b836e3bb3ce03bce4ce1864f2923f6be6df8f
Iishaan Inabathini <iishaan@Pavans-Air.attlocal.net>
2022-02-04 12:19:36 -0600

    small bug

--- selfie/views.py
+++ selfie/views.py
@@ -1,4 +1,4 @@
-.from django.shortcuts import render
+from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
