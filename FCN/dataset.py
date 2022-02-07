637de830b35d890e0f2931b8d09effe9fa6c9cc7
chungus-ii <iishaani@icloud.com>
2022-02-06 19:25:50 -0600

    Adding features, creating train.py

--- FCN/dataset.py
+++ FCN/dataset.py
@@ -1,4 +1,4 @@
-import os, cv2
+import os, cv2, shutil
 from shutil import copy2
 import numpy as np
 import tensorflow as tf
@@ -17,19 +17,23 @@ def split(BASE_PATH = '/eCybermission/selfies', DATASET_PATH = '/eCybermission/d
         The dataset/training and dataset/testing directories are the two directories that will be utilized by data generators.
     """
 
+    #if dataset exists, remove it to create a new one
+    if os.path.exists(DATASET_PATH):
+        shutil.rmtree(DATASET_PATH)
+
     #list of paths of class directories within image directory
     classes = os.listdir(BASE_PATH)
-    
+
     #new directory for dataset
-    os.makedirs(DATASET_PATH, exist_ok=True)
+    os.makedirs(DATASET_PATH)
     
     #create training set directory inside of dataset directory
     training_directory = os.path.join(DATASET_PATH, 'training')
-    os.makedirs(training_directory, exist_ok=True)
+    os.makedirs(training_directory)
     
     #create testing directory inside of dataset directory
     testing_directory = os.path.join(DATASET_PATH, 'testing')
-    os.makedirs(testing_directory, exist_ok=True)
+    os.makedirs(testing_directory)
     
     #copying images from image directory into dataset directory
     for class_name in classes:
@@ -37,11 +41,11 @@ def split(BASE_PATH = '/eCybermission/selfies', DATASET_PATH = '/eCybermission/d
 
         #create directory for current class in testing directory
         class_train = os.path.join(training_directory, class_name)
-        os.makedirs(class_train, exist_ok=True)
+        os.makedirs(class_train)
 
         #create directory for current class in training directory
         class_test = os.path.join(testing_directory, class_name)
-        os.makedirs(class_test, exist_ok=True)
+        os.makedirs(class_test)
 
         #shuffle the image list before seperation
         class_path = os.path.join(BASE_PATH, class_name)
